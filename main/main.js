b5448535c2e7dc20c0133ced4f73b70d287e3061
NgThTam <thanhtm352@gmail.com>
2022-03-01 21:31:58 +0700

    upload

--- main/main.js
+++ main/main.js
@@ -16,3 +16,23 @@ login.addEventListener('click',showModalLogin)
 close_login.addEventListener('click',closeModalLogin)
 modal_login.addEventListener('click',closeModalLogin)
 js_modal.addEventListener('click',modal_event)
+
+//open_close modal register
+
+const register = document.querySelector('.js_register')
+const modal_register = document.querySelector('.modal1')
+const close_register = document.querySelector('.js_close_register')
+const register_contern = document.querySelector('.js_modal_register')
+function showModalRegister(){
+    modal_register.classList.add('open')
+}
+function closeModalRegister(){
+    modal_register.classList.remove('open')
+}
+function event_modal(){
+    event.stopPropagation()
+}
+register.addEventListener('click',showModalRegister)
+close_register.addEventListener('click',closeModalRegister)
+modal_register.addEventListener('click',closeModalRegister)
+register_contern.addEventListener('click',event_modal)
